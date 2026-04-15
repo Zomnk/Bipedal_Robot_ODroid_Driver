b96f943d6dfefde716f92d87f4817c87f549284f
zomnk <2332607882@qq.com>
2026-04-15 19:29:12 +0800

    回复Kp为5.0，更改Kd为0.5

--- tests/test_full_loop.cpp
+++ tests/test_full_loop.cpp
@@ -121,12 +121,12 @@ int main(int argc, char** argv) {
             // 索引顺序: [0-4]左腿 (Yaw, Roll, Pitch, Knee, Ankle)
             //          [5-9]右腿 (Yaw, Roll, Pitch, Knee, Ankle)
             const float kp_gains[NUM_JOINTS] = {
-                8.0f, 20.0f, 8.0f, 8.0f, 8.0f,  // 左腿
-                8.0f, 20.0f, 8.0f, 8.0f, 8.0f   // 右腿
+                5.0f, 20.0f, 5.0f, 5.0f, 5.0f,  // 左腿
+                5.0f, 20.0f, 5.0f, 5.0f, 5.0f   // 右腿
             };
             const float kd_gains[NUM_JOINTS] = {
-                0.4f, 0.4f, 0.4f, 0.4f, 0.4f,  // 左腿
-                0.4f, 0.4f, 0.4f, 0.4f, 0.4f   // 右腿
+                0.5f, 0.5f, 0.5f, 0.5f, 0.5f,  // 左腿
+                0.5f, 0.5f, 0.5f, 0.5f, 0.5f   // 右腿
             };
             const float velocity = 0.0f;
 
